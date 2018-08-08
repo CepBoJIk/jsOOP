9e6f22bbdc525670f4312348293bd9d46aa09f98
Rogov Sergey <cepbojik3007@gmail.com>
2018-08-08 15:12:33 +0300

    check

--- script.js
+++ script.js
@@ -3,12 +3,14 @@
     class WebCompany {
 
         constructor(daysAmount) {
-            this.projectsTypes = ['web', 'mobile'];
-            this.maxDifficult = 3;
+            this._projectsTypes = ['web', 'mobile'];
+            this._maxDifficult = 3;
 
             this.director = new Director();
 
             while( daysAmount > 0 ) {
+                this.director.day++;
+
                 let projects = this.getProjects();
                 
                 this.director.giveProjects(projects);
@@ -16,6 +18,8 @@
                 daysAmount--;
             }
 
+            this.director.logs.getFullLog();
+            console.log(this.director.logs.printFullLog());
         }
 
         getProjects() {
@@ -23,10 +27,10 @@
             let projectsAmount = this.getRandom(0, 5);
 
             while(projectsAmount-- > 0) {
-                let projectTypeNumber = this.getRandom(0, this.projectsTypes.length);
-                let projectDifficult = this.getRandom(1, this.maxDifficult + 1);
+                let projectTypeNumber = this.getRandom(0, this._projectsTypes.length);
+                let projectDifficult = this.getRandom(1, this._maxDifficult + 1);
 
-                let project = new Project(this.projectsTypes[projectTypeNumber], projectDifficult)
+                let project = new Project(this._projectsTypes[projectTypeNumber], projectDifficult)
 
                 projects.push(project);
             }
@@ -41,22 +45,101 @@
 
     }
 
-    /* Класс проекта */
-
     class Project {
 
         constructor(type, difficult) {
+            this.process = {
+                'ACCEPTED': true,
+                'DISTRIBUTED': false,
+                'TESTED': false,
+                'FINISHED': false
+            }
             this.type = type;
             this.difficult = difficult;
         }
 
-        get maxWorker() {
-            if(this.type === 'web') return this.difficult;
-            return 1;
-        }
-
         getDays(workersAmount) {
             return this.difficult / workersAmount;
+        }
+
+        getProcess(process) {
+            return this.process[process];
+        }
+
+        setProcess(process) {
+            for(let i in this.process) {
+                this.process[i] = false;
+            }
+
+            this.process[process] = true;
+        }
+    }
+
+    class LogItem {
+        constructor() {
+            this.day;
+            this.finishedProjects;
+            this.acceptedProjects;
+            this.dismissedDevelopers;
+            this.acceptedDevelopers;
+        }
+
+        get dismissed() {
+            if(this.dismissedDevelopers) {
+                return this.dismissedDevelopers.type;
+            }
+            return 'Не стал увольнять, у него жена, дети...';
+        }
+
+        getAcceptedDevelopersForType(type) {
+            return this.acceptedDevelopers.filter(dev => dev === type);
+        }
+
+        printLog() {
+            return `День ${this.day}, принято проектов ${this.acceptedProjects.length}, завершено проектов ${this.finishedProjects.length}, 
+            принято рабочих ${this.acceptedDevelopers.length}, из них: webDevelopers - ${this.getAcceptedDevelopersForType('web').length}, mobileDevelopers - ${this.getAcceptedDevelopersForType('mobile').length}, qaDevelopers - ${this.getAcceptedDevelopersForType('qa').length}
+            уволен рабочий: ${this.dismissed}`
+        }
+    }
+
+    class Log {
+        constructor() {
+            this.logs = [];
+            this.days = 0;
+            this.finishedProjects = [];
+            this.acceptedProjects = [];
+            this.dismissedDevelopers = [];
+            this.acceptedDevelopers = [];
+        }
+
+        addLog(log) {
+            this.logs.push(log);
+        }
+
+        getFullLog() {
+            this.days = this.logs.length;
+
+            this.getAllProperties('finishedProjects');
+            this.getAllProperties('acceptedProjects');
+
+            this.logs.forEach(log => {
+                if(log.dismissedDevelopers) {
+                    this.dismissedDevelopers.push(log.dismissedDevelopers)
+                }
+            });
+
+            this.getAllProperties('acceptedDevelopers');
+
+        }
+
+        getAllProperties(property) {
+            this.logs.forEach(log => log[property].forEach(p => this[property].push(p)));
+        }
+
+        printFullLog() {
+            return `-------------------------------------------
+            Всего: дней: ${this.days}, принято проектов ${this.acceptedProjects.length}, завершено проектов ${this.finishedProjects.length},
+            уволено рабочих ${this.dismissedDevelopers.length}, принято рабочих ${this.acceptedDevelopers.length} `
         }
     }
 
@@ -64,39 +147,81 @@
 
         constructor() {
             this.needAccept = [];
-
             this.projects = [];
-
+            this.newProjects = [];
+            this.finishedProjectsAtDay = [];
+            this.newDevelopers = [];
+            this.dismissedDeveloper = {};
             this.developers = new Developers();
+            this.logs = new Log();
+            this.day = 0;
+            this.dismissDevelopersInterval = 3;
         }
 
         giveProjects(projects) {
             this.projects = this.projects.concat(projects);
+            this.newProjects = projects;
 
             if(this.needAccept.length) {
-                this.needAccept.forEach((worker, i) => this.developers.acceptWorker(this.needAccept[i]));
+                this.needAccept.forEach((worker) => this.developers.acceptWorker(worker));
+                this.newDevelopers = this.needAccept;
                 this.needAccept = [];
             }
 
+            this.deleteFinishedProjects();
+
             if(this.projects.length != 0) {
-                this.distributeProjects();
+                this.distributeProjects(this.projects);
             }
 
+            this.dismissedDeveloper = this.developers.dismissFreeDevelopers(this.dismissDevelopersInterval);
+
+            this.developers.recalculateDay();
+
+            this.updateLog();
         }
 
-        distributeProjects() {
+        distributeProjects(acceptedProjects) {
+            for(let i = 0; i < acceptedProjects.length; i++) {
 
-            for(let i = 0; i < this.projects.length; i++) {
-
-                if(!this.developers.takeProject(this.projects[i])) {
-                    this.needAccept.push(this.projects[i].type);
+                if(acceptedProjects[i].getProcess('TESTED')) {
+                    acceptedProjects[i].type = 'qa';
+                } 
+                if(!this.developers.takeProject(acceptedProjects[i])) {
+                    this.needAccept.push(acceptedProjects[i].type);
                 } else {
-                    this.projects.splice(i, 1);
-                    --i;    
+                    acceptedProjects[i].setProcess('DISTRIBUTED');
                 }
-
             }
             
+        }
+
+        deleteFinishedProjects() {
+            let finishedProjects = [];
+
+            for(let i = 0; i < this.projects.length; i++) {
+                if(this.projects[i].getProcess('FINISHED')) {
+                    finishedProjects.push(this.projects[i])
+                    this.projects.splice(i, 1);
+                    i--;
+                }
+            }
+
+            this.finishedProjectsAtDay = finishedProjects;
+        }
+
+        updateLog() {
+            
+            let log = new LogItem(this.day, this.finishedProjectsAtDay, this.newProjects, this.dismissedDevelopers);
+            log.day = this.day;
+            log.finishedProjects = this.finishedProjectsAtDay;
+            log.acceptedProjects = this.newProjects;
+            log.dismissedDevelopers = this.dismissedDeveloper;
+            log.acceptedDevelopers = this.newDevelopers;
+                      
+            this.logs.addLog(log);
+
+            console.log(log.printLog());
         }
 
     }
@@ -110,37 +235,120 @@
         }
 
         freeDevelopers(type) {
+            if(!type) {
+                let freeWebDevelopers = this.webDevelopers.filter(dev => dev.free);
+                let freeMobileDeveloper = this.mobileDevelopers.filter(dev => dev.free);
+                let freeQaDevelopers = this.qaDevelopers.filter(dev => dev.free);
+
+                return [].concat(freeWebDevelopers, freeMobileDeveloper, freeQaDevelopers);
+            }
+
             return this[type + 'Developers'].filter( worker => worker.free );
+        }
+
+        busyDevelopers() {
+            let busyWebDevelopers = this.webDevelopers.filter(dev => !dev.free);
+            let busyMobileDevelopers = this.mobileDevelopers.filter(dev => !dev.free);
+            let busyQaDevelopers = this.qaDevelopers.filter(dev => !dev.free);
+
+            return [].concat(busyWebDevelopers, busyMobileDevelopers, busyQaDevelopers);
         }
 
         acceptWorker(workerType) {
             switch(workerType) {
                 case 'web': 
-                    this.webDevelopers.push(new WebDeveloper());
-                break;
+                    this.webDevelopers.push(new WebDeveloper('web'));
+                    break;
                 case 'mobile': 
-                    this.mobileDevelopers.push(new MobileDeveloper());
-                break;
+                    this.mobileDevelopers.push(new MobileDeveloper('mobile'));
+                    break;
                 case 'qa': 
-                    this.qaDevelopers.push(new QADeveloper());
-                break;
+                    this.qaDevelopers.push(new QADeveloper('qa'));
+                    break;
             }
         }
 
         takeProject(project) {
             let projectType = project.type;
+            let freeDevelopers = this.freeDevelopers(projectType);
 
-            if(!this.freeDevelopers(projectType).length) return false;
+            if(!freeDevelopers.length) return false;
+
+            switch(projectType) {
+                case 'web':
+                    freeDevelopers[0].toDoWork(project, project.getDays(1));
+                    break;
+                case 'mobile':
+                    let amountDevelopers = Math.min(freeDevelopers.length, project.difficult);
+
+                    for(let i = 0; i < amountDevelopers; i++) {
+                        freeDevelopers[i].toDoWork(project, project.getDays(amountDevelopers));
+                    }
+                    break;
+                case 'qa':
+                    freeDevelopers[0].toDoWork(project, 1)
+                    break;
+            }
 
             return true;
-            // Код принятия проекта
+        }
+
+        recalculateDay() {
+            let busyDevelopers = this.busyDevelopers();
+            busyDevelopers.forEach(dev => dev.recalculateDaysWork());
+
+            let freeDevelopers = this.freeDevelopers();
+            freeDevelopers.forEach(dev => dev.freeDays++);
+        }
+
+        dismissFreeDevelopers(freeDaysAmount) {
+            let dismiss = this.freeDevelopers().filter(dev => dev.freeDays > freeDaysAmount);
+            let dismissed = {};
+            let dismissedIndex;
+
+            if(!dismiss.length) return false;
+
+            dismiss.sort((dev1, dev2) => dev1.projectsAmount - dev2.projectsAmount);
+            dismissed = dismiss[0];
+
+            dismissedIndex = this[dismissed.type + 'Developers'].indexOf(dismissed);
+
+            if(dismissedIndex != -1) {
+                this[dismissed.type + 'Developers'].splice(dismissedIndex, 1);
+                return dismissed;
+            }
+
+            return false;
         }
     }
 
     class Developer {
+        constructor(type) {
+            this.type = type;
+            this.daysWork = 0;
+            this.project = {};
+            this.projectsAmount = 0;
+            this.freeDays = 0;
+        }
 
-        constructor() {
-            this.free = true;
+        get free() {
+            return this.daysWork < 1;
+        }
+
+        toDoWork(project, days) {
+            this.project = project;
+            this.daysWork = days;
+            this.freeDays = 0;
+        }
+
+        recalculateDaysWork() {
+            this.daysWork--;
+
+            if(this.free) {
+                this.project.setProcess('TESTED');
+                this.project = {};
+                this.projectsAmount++;
+            }
         }
 
     }
@@ -149,8 +357,8 @@
 
     class WebDeveloper extends Developer {
 
-        constructor() {
-            super();
+        constructor(type) {
+            super(type);
 
         }
 
@@ -158,8 +366,8 @@
 
     class MobileDeveloper extends Developer {
 
-        constructor() {
-            super()
+        constructor(type) {
+            super(type);
 
         }
 
@@ -167,13 +375,22 @@
 
     class QADeveloper extends Developer {
 
-        constructor() {
-            super()
+        constructor(type) {
+            super(type);
 
+        }
+
+        recalculateDaysWork() {
+            this.daysWork--;
+
+            if(this.free) {
+                this.project.setProcess('FINISHED');
+                this.project = {};
+            }
         }
 
     }
 
-    new WebCompany(5);
+    new WebCompany(10);
 
 })()
